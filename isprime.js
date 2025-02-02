e663d2da822c8aa3e6b3cedd7788a689c3f2da0b
mahdi <mahdi.amini8894@gmail.com>
2025-02-02 13:43:15 +0330

    final

--- isprime.js
+++ isprime.js
@@ -9,6 +9,9 @@
 //     return
 //     }}
 
+
+
+
 // isprime(53)
 // function convert(hour){
 //     if(12<hour<=24){
@@ -17,29 +20,42 @@
 //     }
 // }
 // convert(23)
-const range = (1,2,3,4 ,5,6,7,8,9,10)
-function inrange(num){
-    for( const element of range){
-       if( num == element){
+
+
+
+
+ function inrange(num,min,max){
+    if( num>=min && num <= max){
     console.log("true")
     }
-   }
 }
-inrange(8)
+
+inrange(8,22,20)
 /////////////////////
-function bmi(weight,height){
-    const result = weight / height**2
-    if( 16<=result<18.5)
-        alert(underweight)
-    else if(18.5<=result<25)
-        alert(normalweight)
-    else if(25<=result<30)
-        alert(overweight)
-    else if(30<=result)
-        alert(obesity)
-}
-bmi(82,2)
+
+
+
+
+
+
+
+// function bmi(weight,height){
+//     const result = weight / height**2
+//     if( 16<=result<18.5)
+//         alert(underweight)
+//     else if(18.5<=result<25)
+//         alert(normalweight)
+//     else if(25<=result<30)
+//         alert(overweight)
+//     else if(30<=result)
+//         alert(obesity)
+// }
+// bmi(82,2)
 ///////////////////
+
+
+
+
 
 // function leap(num){
 //     if( num%100==0 && num%400==0){
@@ -51,19 +67,28 @@ bmi(82,2)
 // }
 // leap(2024)
 /////////////////
-function Vowel(str){
-    const vowels =["a", "e","i","o","u","A","E","O","U"]
-    volwes.test(str)
-        console.log(true)
-    }
-Vowel("hello")
-////////////////
-function reverse(str) {
-    const re = reverse()
-    console.log(re)
-}
 
-reverse("hello")
+
+
+
+// function Vowel(str){
+//     const vowel =["a", "e","i","o","u","A","E","O","U"]
+//     vowel.test(str)
+//         console.log(true)
+//     }
+// Vowel("hello")
+////////////////
+
+
+
+
+
+// function reversstring(str) {
+//     const re = str.split().reverse().joint()
+//     console.log(re)
+// }
+
+// reversestring("hello")
 
 
 
